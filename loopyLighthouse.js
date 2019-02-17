5ed9a34a2a50a12b06cd0da990700cbb02f54193
Francesco Valela <valela.francesco@gmail.com>
2019-02-17 13:31:00 -0500

    small stylistic and security changes
    Javascript is moving away from var to let and const. 
    - const is for variables that will not change (called immutable). I put them as global scope (i.e. at the top of the file, not enclosed by curly braces) as const variables are typically put there (varies depending on usage). 
    - let are for variables that can change (called mutable). 
    
    We can look into more differences between let and var in future discussions.

--- loopyLighthouse.js
+++ loopyLighthouse.js
@@ -3,11 +3,12 @@
 // 1. If the number is multiple of 3, print the string "Loopy" instead of the number
 // 2. If the number is a multiple of 4, print the string "Lighthouse" instead of the number
 // 3. If the number is a multiple of both 3 and 4, print the string "LoopyLighthouse"
+
+const startingNumber = 100;
+const endingNumber = 200;
+
 function loopyLighthouse () {
-  var startingNumber = 100;
-  var endingNumber = 200;
-  for (var i = 0; i <= endingNumber - startingNumber; i++) {
-    var currentNumber = startingNumber + i;
+  for (let currentNumber = startingNumber; currentNumber <= endingNumber; currentNumber++) {
     if (currentNumber % 3 === 0 && currentNumber % 4 === 0) {
       console.log("LoopyLighthouse");
     } else if (currentNumber % 3 === 0) {
